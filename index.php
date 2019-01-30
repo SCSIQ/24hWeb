90d425cb512a8c3bd5707b08a63b895813dd2ea3
Aurian <durand.aurian@me.com>
2019-01-30 09:17:00 +0100

    Création index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Test Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    <?php
+    // ajouter le fichier php ou html a appeler
+    //require '';
+    ?>
+    
+</body>
+</html>
