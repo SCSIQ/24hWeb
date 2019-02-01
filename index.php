2169c959a8c47108ddc83f877e2171b5b7a65976
Aurian <durand.aurian@me.com>
2019-02-01 13:35:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Test Login</title>
+    <title>WSC'Event</title>
+    <!--
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
+    -->
+    <link rel="stylesheet" type="text/css" href="css/menu.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
 
